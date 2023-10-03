f75a8a829920d7a0452b95e4da889c074c60d541
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-03 19:37:42 +0530

    Add files via upload

---
+++ 4_Square_of_number.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ //1 4 9 16 25
+ for(int row = 1; row<=5; row++){
+    for(int col = 1; col<=5; col++){
+        cout<<col*col<<" ";
+    }
+    cout<<endl;
+ }
+ return 0;
+}
