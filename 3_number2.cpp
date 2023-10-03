7653da6c8860b65edd2b7f2d8ffd4d338cc7596d
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-03 19:36:41 +0530

    Add files via upload

---
+++ 3_number2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ for(int row=1; row<=5; row++){
+    for(int col = 5; col>=1; col--){
+        cout<<col<<" ";
+    }
+    cout<<endl;
+ }
+ return 0;
+}
