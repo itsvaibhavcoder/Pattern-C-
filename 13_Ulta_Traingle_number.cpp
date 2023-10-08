2d46ef1e4b8cbd8ddab5775b63e5e4b6776c5e81
vaibhav sukale <104653559+itsvaibhavcoder@users.noreply.github.com>
2023-10-09 02:31:47 +0530

    Add files via upload

---
+++ 13_Ulta_Traingle_number.cpp
@@ -0,0 +1,21 @@
+/*
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ for(int row=1; row<=5; row++){
+    for(int col = 1; col<=5-(row-1); col++){
+        cout<<col<<" ";
+    }
+    cout<<endl;
+ }
+ return 0;
+}
